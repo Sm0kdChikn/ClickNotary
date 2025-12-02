f4202ccfba038e06615a219558cd4811588c89cd
BrandonDenning <brdenning3@gmail.com>
2025-12-01 18:06:50 -0700

    more changes 12-1-25

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Simple smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Optional: Add subtle fade-in on scroll later
+console.log("ClickNotary script loaded");
